8bacf32a205d2cc571bc6ca0a7c73acfba2640b9
govind2602 <160204238+govind2602@users.noreply.github.com>
2024-08-26 23:22:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+ 
+         function openGoogleForm() {
+            window.open("https://docs.google.com/forms/d/e/1FAIpQLScqM76Ku0x9oDka_oebRGQVFCCcP1mTR_uvVmlSeSuMGHVy2w/viewform?embedded=true", "_blank");
+        }
+        
         var typed = new Typed('#element', {
             strings: ['Web Developer', 'Problem solver', 'Web designer', 'Video Editor'],
             typeSpeed: 50,
